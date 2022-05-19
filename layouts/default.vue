6aae11e6bf25b3b0958fe8855e10ff11c905492c
siavava <amittaijoel@outlook.com>
2022-05-19 00:01:20 -0400

    Checkpoint, css

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,12 +25,15 @@ export default {
     padding: 0
 
   body
-    font-family: aktiv-grotesk, courier-new, courier, monospace
+    // font-family: aktiv-grotesk, courier-new, courier, monospace
+    font-family: 'Mercury SSm A', 'Mercury SSm B', georgia, serif
     font-style: normal
-    font-weight: 400
-    font-size: 1rem
-    line-height: 1.5
+    font-weight: 300
+    font-size: 1em
+    line-height: 1.85em
     background: #f4f4f4
+    color: #003F33
+    background-color: #edece8
 
 
   a
