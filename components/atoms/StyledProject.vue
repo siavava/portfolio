f4d3b7790252aef01d23b04131cf724ff66d2546
siavava <amittaijoel@outlook.com>
2023-01-07 04:25:28 -0500

    #fix -- featured proj image width/height

--- components/atoms/StyledProject.vue
+++ components/atoms/StyledProject.vue
@@ -80,7 +80,12 @@
       @media (max-width: 768px)
         grid-column: 1 / -1
         height: 100%
-        max-height: 100%
+
+        :is(a, img)
+          width: 100%
+          height: 100%
+          max-width: 100%
+          max-height: 100%
       
     
   
