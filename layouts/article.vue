8dad48cf2f098ec14927404d4a4ff7d3886589b0
siavava <amittaijoel@outlook.com>
2023-06-06 20:45:42 -0400

    #bug-test

--- layouts/article.vue
+++ layouts/article.vue
@@ -71,11 +71,16 @@ onMounted(() => {
 </script>
 
 <style lang="sass" scoped>
-@use "../styles/default"
+//@use "../styles/default"
 @use "../styles/typography"
 @use "../styles/colors"
 @use "../styles/geometry"
 @use "../styles/mixins"
+
+*, *::before, *::after
+  outline: 1px solid red !important
+  opacity: 1 !important
+  visibility: visible !important
 
 .article-main
   width: min(100%, 1300px)
