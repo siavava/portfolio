d26769b9e733692c021a9f9e6af21151eba61554
siavava <amittaijoel@outlook.com>
2023-06-12 14:19:59 -0400

    clean up firebase

--- plugins/firebase.auth.ts
+++ plugins/firebase.auth.ts
@@ -4,21 +4,6 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export default defineNuxtPlugin((nuxtApp) => {
-// TODO: Move firebase config to global config file.
-
-  const config = useRuntimeConfig();
-
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyCtHIhBCYkQeafAn_ICQowTWumlPRwxkU0",
-  //   authDomain: "altair-on-the-web.firebaseapp.com",
-  //   projectId: "altair-on-the-web",
-  //   storageBucket: "altair-on-the-web.appspot.com",
-  //   messagingSenderId: "470937183370",
-  //   appId: "1:470937183370:web:d99db4a8ca1ee3ac3e3973",
-  //   measurementId: "G-7DGSTFLF85",
-  // };
-
-  initializeApp(config.firebaseConfig);
-
-  // nuxtApp.datastore = app;
+  const { firebaseConfig } = useRuntimeConfig();
+  initializeApp(firebaseConfig);
 });
