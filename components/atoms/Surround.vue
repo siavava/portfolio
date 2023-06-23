ce6f10bc62e0f9199023598b70d45af9977e41d0
siavava <amittaijoel@outlook.com>
2023-06-23 01:35:00 -0400

    l

--- components/atoms/Surround.vue
+++ components/atoms/Surround.vue
@@ -39,7 +39,7 @@ console.log(`path: ${JSON.stringify(useRoute().path)}`);
 const { data: surround } = await useAsyncData(
   // "prev-next",
   async () => {
-    const surround = await queryContent(path)
+    const surround = await queryContent()
       .where({ draft: false })
       .only(["_path", "title", "category", "date"])
       .sort({ date: -1 })
