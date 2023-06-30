fc82d7213d8c4ea215f689f9ebe31e4d390d0ea3
siavava <amittaijoel@outlook.com>
2023-06-30 16:13:35 -0400

    #update: lighthouse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,9 @@ export default defineNuxtConfig({
 
     head: {
       title: "whatever",
+      htmlAttrs: {
+        lang: "en",
+      },
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
