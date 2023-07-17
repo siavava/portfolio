8a871250b76b4a4dfdd495dfad9a7038c3e2e948
siavava <amittaijoel@outlook.com>
2023-07-17 19:51:10 -0400

    #bump: versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,9 +40,9 @@ export default defineNuxtConfig({
         {
           rel: "apple-touch-icon", type: "image/svg", href: "/favicon.svg",
         },
-        // {
-        //   rel: "manifest", href: "/manifest.json",
-        // },
+        {
+          rel: "manifest", href: "/manifest.json",
+        },
       ],
     },
   },
