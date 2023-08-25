8901174b26eadf4c09a11b5e97877ed09a8f1e93
siavava <amittaijoel@outlook.com>
2023-08-25 05:40:20 -0400

    mobile flex

--- components/content/ProseA.vue
+++ components/content/ProseA.vue
@@ -20,30 +20,30 @@ defineProps({
     class="link-wrapper"
     :style="{
       'margin-bottom': fancy ? '1em' : '0',
-      'display': fancy ? 'inline-flex' : 'inline'
     }"
   >
+    <!-- 'display': fancy ? 'inline-flex' : 'inline' -->
     <NuxtLink
       :href="href"
       :class="{'link': true, 'arrow': fancy}"
     >
-      <span>
-        <slot />
-      </span>
+      <slot />
     </NuxtLink>
-    <div
+    <!-- <span
       v-if="fancy"
       class="pointer"
+    > -->
+    <svg
+      v-if="fancy"
+      class="pointer"
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5
+      <path
+        d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5
             4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8
             8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645
             8.64645C2.45118 8.84171 2.45118 9.15829 2.64645
@@ -51,10 +51,10 @@ defineProps({
             9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8
             3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645
             8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
-          fill="#eee"
-        />
-      </svg>
-    </div>
+        fill="#eee"
+      />
+    </svg>
+    <!-- </span> -->
   </div>
 </template>
 
@@ -66,6 +66,7 @@ defineProps({
   align-items: center
   gap: 7px
   display: inline
+  background: rgba(yellow, 0.1)
 
   &:hover
     border-bottom: 0
@@ -78,14 +79,19 @@ defineProps({
   line-height: 0.9em
   display: inline
 
-  &:hover > span
-    border-bottom: 1px solid
-    display: inline
+  a
+    border-bottom: 1px solid transparent
 
     &:not(:last-child)
-      margin-right: 0.5em
+      margin-right: 7px
 
-  &.arrow
-    height: 1em
+  &:hover > a
+    border-bottom: 1px solid
 
+.pointer
+  height: 1em
+  margin: 0 !important
+  aspect-ratio: 1/1 !important
+  padding: 0 !important
+  display: inline
 </style>
